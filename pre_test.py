53e9279e526b56545b4e10191a2702e4b77d9014
moskovskayaliza2002 <moscovskayaliza@gmail.com>
2022-04-14 16:19:14 +0300

    add drawing bboxes

--- pre_test.py
+++ pre_test.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 from torchvision.io import read_image 
 from torchvision.utils import draw_bounding_boxes
 from datetime import datetime
-#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
+#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
 
 # return FloatTensor of boxes for each file in directory
 def read_boxes(directory, indexes):
