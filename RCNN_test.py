638a93b7568bac7ab751c1853b5d2eb2fcc38652
moskovskayaliza2002 <moscovskayaliza@gmail.com>
2022-06-28 16:57:47 +0300

    update comments in functions

--- RCNN_test.py
+++ RCNN_test.py
@@ -1,6 +1,6 @@
 import argparse
 from torch.utils.data import Dataset, DataLoader
-from RCNN_model import ClassDataset, get_model, visualize
+from RCNN_model import ClassDataset, get_model
 import cv2
 import torch
 import glob
@@ -10,6 +10,7 @@ from torchvision.transforms import functional as F
 import numpy as np
 import torchvision
 from crop_into4 import crop_one_im, read_boxes
+import matplotlib.pyplot as plt
 
 
 def ver(bboxes, kp, l1, l2, r1, r2, crop_w, crop_h): 
@@ -66,19 +67,20 @@ def test_one_from4(im_path, model, flag, nms_threshold, iou_threshold):
     input_im = F.to_tensor(input_im)
     orig_bb = []
     orig_kp = []
-    if flag:
-        number = im_path[im_path.rfind('e') + 1 : im_path.rfind('.')]
-        im_root = im_path[:im_path.rfind('/')]
-        test_root = im_root[:im_root.rfind('/') + 1]
-        b_path = test_root + 'bboxes/bbox' + number + '.txt'
-        orig_bb = read_boxes(b_path)
-        k_path = test_root + 'keypoints/keypoint' + number + '.txt'
-        kp = read_boxes(k_path)
-        for i in kp:
-            orig_kp.append([[i[0], i[1]], [i[2], i[3]]])
-        
+    # Получаем настоящие координаты для целого изображения
+    number = im_path[im_path.rfind('e') + 1 : im_path.rfind('.')]
+    im_root = im_path[:im_path.rfind('/')]
+    test_root = im_root[:im_root.rfind('/') + 1]
+    b_path = test_root + 'bboxes/bbox' + number + '.txt'
+    orig_bb = read_boxes(b_path)
+    k_path = test_root + 'keypoints/keypoint' + number + '.txt'
+    kp = read_boxes(k_path)
+    for i in kp:
+        orig_kp.append([[i[0], i[1]], [i[2], i[3]]])
+    # Разрезаем изображение на 4    
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     left_1, left_2, right_1, right_2, c_w, c_h = crop_one_im(img)
+    # Проверяем что не пересекаем боксы линиями разреза и получаем предсказания для каждого нового изображения и объединяем их
     if ver(orig_bb, kp, left_1, left_2, right_1, right_2, c_w, c_h):
         with torch.no_grad():
             model.to(device)
@@ -92,9 +94,11 @@ def test_one_from4(im_path, model, flag, nms_threshold, iou_threshold):
         kp_l2, bb_l2 = get_out_kp_bb(out_l2, 0, c_h, kp_l1, bb_l1)
         kp_r1, bb_r1 = get_out_kp_bb(out_r1, c_w, 0, kp_l2, bb_l2)
         kp_r2, bb_r2 = get_out_kp_bb(out_r2, c_w, c_h, kp_r1, bb_r1)
-          
+         
+        # Визуализация и предсказаний, и таргетов 
         if flag:
             visualize(img, bb_r2, kp_r2, img, orig_bb, orig_kp)
+        # Визуализация только таргетов
         else:
             visualize(img, bb_r2, kp_r2)
     else:
@@ -139,7 +143,9 @@ def test_batch_from4(model, root, flag, nms_threshold, iou_threshold):
             print(f"number {number}")
             orig_bbox = find_bbox(number, root)
             orig_kp, kp = find_kp(number, root)
+            # Разрезаем изображение на 4    
             left_1, left_2, right_1, right_2, c_w, c_h = crop_one_im(original_image)
+            # Проверяем, что не пересекаем боксы линиями разреза и получаем предсказания для каждого нового изображения и объединяем их
             if ver(orig_bbox, kp, left_1, left_2, right_1, right_2, c_w, c_h):
                 with torch.no_grad():
                     model.to(device)
@@ -154,64 +160,92 @@ def test_batch_from4(model, root, flag, nms_threshold, iou_threshold):
                 kp_r1, bb_r1 = get_out_kp_bb(out_r1, c_w, 0, kp_l2, bb_l2)
                 kp_r2, bb_r2 = get_out_kp_bb(out_r2, c_w, c_h, kp_r1, bb_r1)
                 
+                # Визуализация и предсказаний, и таргетов
                 if flag:
                     visualize(original_image, bb_r2, kp_r2, original_image, orig_bbox, orig_kp)
-                
+                # Визуализация только таргетов
                 else:
                     visualize(original_image, bb_r2, kp_r2)
             else:
                 print(f"Изображение {f.path} не подходит")
+ 
+
+def visualize(image, bboxes, keypoints, image_original=None, bboxes_original=None, keypoints_original=None):
+    # Рисует на изображении предсказанные и настоящие боксы и ключевые точки.
+    fontsize = 12
+    keypoints_classes_ids2names = {0: 'A', 1: 'H'}
+    for bbox in bboxes:
+        start_point = (bbox[0], bbox[1])
+        end_point = (bbox[2], bbox[3])
+        image = cv2.rectangle(image.copy(), start_point, end_point, (255,0,0), 2)
     
-def test_batch(model, data_loader_test, flag, nms_threshold, iou_threshold, dir_size): # Некорректно работает
-    for i in range(dir_size):
-        iterator = iter(data_loader_test)
-        images, targets = next(iterator)
-        images = list(image.to(device) for image in images)
+    for kps in keypoints:
+        for idx, kp in enumerate(kps):
+            image = cv2.circle(image.copy(), tuple(kp), 2, (255,0,0), 10)
+            image = cv2.putText(image.copy(), " " + keypoints_classes_ids2names[idx], tuple(kp), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 1, cv2.LINE_AA)
+    if image_original is None and keypoints_original is None:
+        plt.figure(figsize=(40,40))
+        plt.imshow(image)
+        plt.show(block=True)
 
-        with torch.no_grad():
-            model.to(device)
-            model.eval()
-            output = model(images)
-                
-        image = (images[0].permute(1,2,0).detach().cpu().numpy() * 255).astype(np.uint8)
-            
-        if flag:
-            gt_bboxes = targets[0]["boxes"]
-            gt_kps = targets[0]["keypoints"]
-            #gt_kps = torch.squeeze(gt_kps)
-            for i in range(gt_bboxes.size(dim=0)):
-                x_min, y_min, x_max, y_max = int(gt_bboxes[i][0].item()), int(gt_bboxes[i][1].item()), int(gt_bboxes[i][2].item()), int(gt_bboxes[i][3].item())
-                image = cv2.rectangle(image.copy(),(x_min,y_min),(x_max,y_max),(50,205,50),1)
-                x_center_body, y_center_body = int(gt_kps[i][0][0].item()), int(gt_kps[i][0][1].item())
-                x_center_head, y_center_head = int(gt_kps[i][1][0].item()), int(gt_kps[i][1][1].item())
-                image = cv2.circle(image.copy(), (x_center_body, y_center_body), 2, (50,205,50), -1)
-                image = cv2.putText(image.copy(),'A', (x_center_body + 2, y_center_body + 2), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (50,205,50), 1)
-                image = cv2.circle(image.copy(), (x_center_head, y_center_head), 2, (50,205,50), -1)
-                image = cv2.putText(image.copy(),'H', (x_center_head + 2, y_center_head + 2), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (50,205,50), 1)
-            
-        scores = output[0]['scores'].detach().cpu().numpy()
+    else:
+        for bbox in bboxes_original:
+            start_point = (bbox[0], bbox[1])
+            end_point = (bbox[2], bbox[3])
+            image_original = cv2.rectangle(image_original.copy(), start_point, end_point, (0,255,0), 2)
+        
+        for kps in keypoints_original:
+            for idx, kp in enumerate(kps):
+                image_original = cv2.circle(image_original, tuple(kp), 2, (0,255,0), 10)
+                image_original = cv2.putText(image_original, " " + keypoints_classes_ids2names[idx], tuple(kp), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1, cv2.LINE_AA)
 
-        high_scores_idxs = np.where(scores > nms_threshold)[0].tolist() # Indexes of boxes with scores > 0.7
-        post_nms_idxs = torchvision.ops.nms(output[0]['boxes'][high_scores_idxs], output[0]['scores'][high_scores_idxs], iou_threshold).cpu().numpy() # Indexes of boxes left after applying NMS (iou_threshold=0.3)
+        f, ax = plt.subplots(1, 2, figsize=(40, 20))
 
-        keypoints = []
-        for kps in output[0]['keypoints'][high_scores_idxs][post_nms_idxs].detach().cpu().numpy():
-            keypoints.append([list(map(int, kp[:2])) for kp in kps])
+        ax[0].imshow(image_original)
+        ax[0].set_title('Original image', fontsize=fontsize)
 
-        bboxes = []
-        for bbox in output[0]['boxes'][high_scores_idxs][post_nms_idxs].detach().cpu().numpy():
-            bboxes.append(list(map(int, bbox.tolist())))
-            
-        visualize(image, bboxes, keypoints)
+        ax[1].imshow(image)
+        ax[1].set_title('Transformed image', fontsize=fontsize)
+        plt.show(block=True)
+
+ 
+def visualizate_predictions(model, data_loader_test, nms_threshold, iou_threshold):
+    # Визуализирует предсказания для батча. Для модели, что учится на целиковых изображениях.
+    iterator = iter(data_loader_test)
+    images, targets = next(iterator)
+    images = list(image.to(device) for image in images)
+
+    with torch.no_grad():
+        model.to(device)
+        model.eval()
+        output = model(images)
+        
+    image = (images[0].permute(1,2,0).detach().cpu().numpy() * 255).astype(np.uint8)
+    scores = output[0]['scores'].detach().cpu().numpy()
+
+    high_scores_idxs = np.where(scores > nms_threshold)[0].tolist() # Indexes of boxes with scores > 0.7
+    post_nms_idxs = torchvision.ops.nms(output[0]['boxes'][high_scores_idxs], output[0]['scores'][high_scores_idxs], iou_threshold).cpu().numpy() # Indexes of boxes left after applying NMS (iou_threshold=0.3)
+
+    keypoints = []
+    for kps in output[0]['keypoints'][high_scores_idxs][post_nms_idxs].detach().cpu().numpy():
+        keypoints.append([list(map(int, kp[:2])) for kp in kps])
+
+    bboxes = []
+    for bbox in output[0]['boxes'][high_scores_idxs][post_nms_idxs].detach().cpu().numpy():
+        bboxes.append(list(map(int, bbox.tolist())))
+    
+    visualize(image, bboxes, keypoints)
         
         
 
-def test_of_single_image(im_path, model, flag, nms_threshold, iou_threshold): # Работает правильно, но можно исправить рисование gt таргетов.
+def test_of_single_image(im_path, model, flag, nms_threshold, iou_threshold):
+    # Тестирование на одной изображении. Для модели, что учится на целиковых изображениях.
     img = cv2.imread(im_path, cv2.IMREAD_UNCHANGED)
     input_im = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     input_im = F.to_tensor(input_im)
     print(input_im.size())
-    
+    bb_orig = []
+    kp_orig = []
     if flag:
         print(im_path.rfind('e'), im_path.rfind('.'))
         number = im_path[im_path.rfind('e') + 1 : im_path.rfind('.')]
@@ -225,15 +259,12 @@ def test_of_single_image(im_path, model, flag, nms_threshold, iou_threshold): # 
         with open(b_path) as f:
             for i in f:
                 x_min, y_min, x_max, y_max = map(int, i[:-1].split(' '))
-                img = cv2.rectangle(img,(x_min,y_min),(x_max,y_max),(50,205,50),1)
+                bb_orig.append([x_min, y_min, x_max, y_max])
         
         with open(k_path) as f:
             for i in f:
                 x_center_body, y_center_body, x_center_head, y_center_head = map(int, i[:-1].split(' '))
-                img = cv2.circle(img, (x_center_body, y_center_body), 2, (50,205,50), -1)
-                img = cv2.putText(img,'A', (x_center_body + 2, y_center_body + 2), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (50,205,50), 1)
-                img = cv2.circle(img, (x_center_head, y_center_head), 2, (50,205,50), -1)
-                img = cv2.putText(img,'H', (x_center_head + 2, y_center_head + 2), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (50,205,50), 1)
+                kp_orig.append([[x_center_body, y_center_body], [x_center_head, y_center_head]])
     
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     with torch.no_grad():
@@ -252,17 +283,20 @@ def test_of_single_image(im_path, model, flag, nms_threshold, iou_threshold): # 
     bboxes = []
     for bbox in output[0]['boxes'][high_scores_idxs][post_nms_idxs].detach().cpu().numpy():
         bboxes.append(list(map(int, bbox.tolist())))
-        
-    visualize(img, bboxes, keypoints)
+    
+    if flag:
+        visualize(img, bboxes, keypoints, img, bb_orig, kp_orig)
+    else:
+        visualize(img, bboxes, keypoints)
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     #parser.add_argument('test_data_path', nargs='?', default='/home/ubuntu/ant_detection/TEST_ACC_DATA/images/0a302e52-image202.png', help="Specify the path either to the folder with test images to test everything, or the path to a single image", type=str)
     parser.add_argument('test_data_path', nargs='?', default='/home/ubuntu/ant_detection/TEST_ACC_DATA', help="Specify the path either to the folder with test images to test everything, or the path to a single image", type=str)
-    parser.add_argument('model_path', nargs='?', default='/home/ubuntu/ant_detection/rcnn_models/20220627-144837/best_weights.pth', help="Specify weights path", type=str)
-    parser.add_argument('draw_targets', nargs='?', default=False, help="True - will draw targets, False - will not", type=bool)
-    parser.add_argument('nms_threshold', nargs='?', default=0.1, help="Non maximum supression threshold for boxes", type=float)
-    parser.add_argument('iou_threshold', nargs='?', default=0.1, help="IOU threshold for boxes", type=float)
+    parser.add_argument('model_path', nargs='?', default='/home/ubuntu/ant_detection/rcnn_models/20220628-124306/best_weights.pth', help="Specify weights path", type=str)
+    parser.add_argument('draw_targets', nargs='?', default=True, help="True - will draw targets, False - will not", type=bool)
+    parser.add_argument('nms_threshold', nargs='?', default=0.5, help="Non maximum supression threshold for boxes", type=float)
+    parser.add_argument('iou_threshold', nargs='?', default=0.3, help="IOU threshold for boxes", type=float)
     
     args = parser.parse_args()
     test_data_path = args.test_data_path
@@ -279,6 +313,5 @@ if __name__ == '__main__':
         test_one_from4(test_data_path, test_model, draw_targets, nms_threshold, iou_threshold)
         
     else:
-        print('ya tyt')
-        #test_batch(test_model, data_loader, draw_targets, nms_threshold, iou_threshold, dir_size)
+        #visualizate_predictions(test_model, data_loader, nms_threshold, iou_threshold)
         test_batch_from4(test_model, test_data_path, draw_targets, nms_threshold, iou_threshold)
